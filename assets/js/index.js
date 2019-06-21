88e5d8acf959a2b5dcf11be833ccf94afba7e751
quentin.picart <quentin.picart@gmail.com>
2019-06-21 12:41:13 +0200

    refactor: display 12 random heroes

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,14 +3,16 @@ const listHeroes = "https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api//all.j
 
 ajaxGet(listHeroes, function(response) {
     const heroesElt = document.getElementById("heroes");
-    const heroes = JSON.parse(response);
+    let heroes = JSON.parse(response);
+    
+    heroes = getRandHeroes(heroes, 12);
 
     heroes.forEach(hero => {
-        
         const name = hero.name;
         const powerStats = hero.powerstats;
         const imageSM = hero.images.sm;
 
+        
         const contentElt = document.createElement("div");
         contentElt.classList.add("col-md-2");
 
@@ -39,3 +41,19 @@ ajaxGet(listHeroes, function(response) {
         heroesElt.appendChild(contentElt);
     });
 });
+
+/*
+param heroes[]
+param int number
+return heroes[] 
+with returned heroes.length === number
+*/
+function getRandHeroes(heroes, number) {
+    const heroesLength = heroes.length;
+    const randHeroes = [];
+
+    for (let i = 0; i < number; i++) {
+        randHeroes.push(heroes[Math.floor((Math.random() * heroesLength) + 1)]);
+    }
+    return randHeroes;
+}
